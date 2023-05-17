b0c9460c25abb59e62f3a193fbd05006d65c875f
Yuhao Zhang <yz7726@nyu.edu>
2023-05-16 22:39:59 -0400

    finish single node

--- network.go
+++
@@ -1,7 +0,0 @@
-package main
-
-type version struct {
-	Version    int    // 版本号
-	BestHeight int    // 区块链高度
-	AddrFrom   string // 发送者的地址 当一个节点接收到 version 消息，它会检查本节点的区块链是否比 BestHeight 的值更大。如果不是，节点就会请求并下载缺失的块
-}
